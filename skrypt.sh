b493bc2548828c1b4154796057aaf048374b9cf9
aatec13 <saganzuzia@gmail.com>
2025-06-17 18:01:54 +0200

    Tworzenie 100 plikow log.txt

--- skrypt.sh
+++ skrypt.sh
@@ -3,3 +3,15 @@
 if [ "$1" == "--date" ]; then
   echo "Data: $(date)"
 fi
+
+if [ "$1" == "--logs" ]; then
+  num_files=100
+  if [ -n "$2" ]; then
+    num_files=$2
+  fi
+  
+  for ((i=1; i<=num_files; i++)); do
+    echo "log${i}.txt - skrypt.sh - $(date)" > "log${i}.txt"
+  done
+  echo "Utworzono $num_files plików log."
+fi
